c4d723e32869e3b0fd4f7654ceea4592b1fb72ef
YairIvgi <33713811+YairIvgi@users.noreply.github.com>
2017-11-21 19:22:42 +0200

    Update ReadAndWriteWithFilter.java

--- WIfi_Location2/src/ReadAndWriteWithFilter.java
+++ WIfi_Location2/src/ReadAndWriteWithFilter.java
@@ -63,46 +63,4 @@ public class ReadAndWriteWithFilter {
 		// We are done
 		System.out.println("The kml file was generated.");
 	}
-
-
-	public static void main(String[] args) throws IOException {
-//	    // Style
-//	    PolyStyle polystyle = KmlFactory.createPolyStyle();
-//	    polystyle.setColor("#EABCFF");
-//	    // polystyle.setFill(true);
-//	    polystyle.setOutline(false);
-//	    //
-//	    Kml kml = KmlFactory.createKml();
-//	    Document document = kml.createAndSetDocument();
-//	    Placemark pm = document.createAndAddPlacemark();
-//	    LinearRing linearRing = pm.createAndSetPolygon().createAndAddInnerBoundaryIs().createAndSetLinearRing();
-//	    linearRing.addToCoordinates(9.184254, 45.443636, 0);
-//	    linearRing.addToCoordinates(9.183379, 45.434288, 0);
-//	    linearRing.addToCoordinates(9.224836, 45.431499, 0);
-//	    linearRing.addToCoordinates(9.184254, 45.443636, 0);
-//	    pm.createAndAddStyle().setPolyStyle(polystyle);
-//	    //
-//	    kml.marshal(new FileWriter("D:/prova.kml"));
-		// The all encapsulating kml element.
-		Kml kml = KmlFactory.createKml();
-		// Create <Placemark> and set values.
-		Placemark placemark = KmlFactory.createPlacemark();
-		placemark.setName("Java User Group Hessen - JUGH!");
-		placemark.setVisibility(true);
-		placemark.setOpen(false);
-		placemark.setDescription("die Java User Group Hessen");
-		placemark.setStyleUrl("styles.kml#jugh_style");
-		 
-		// Create <Point> and set values.
-		Point point = KmlFactory.createPoint();
-		point.setExtrude(false);
-		point.setAltitudeMode(AltitudeMode.CLAMP_TO_GROUND);
-		// Add <coordinates>9.444652669565212,51.30473589438118,0<coordinates>.
-		point.getCoordinates().add(new Coordinate("9.444652669565212,51.30473589438118,0"));
-		 
-		placemark.setGeometry(point);      // <-- point is registered at placemark ownership.
-		kml.setFeature(placemark);         // <-- placemark is registered at kml ownership.
-		kml.marshal(System.out);           // <-- Print the KML structure to the console.
-	}
-	
 }
