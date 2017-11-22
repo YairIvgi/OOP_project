04a983c72baa4cd144bc3eb655753aa5fba227f1
YairIvgi <yair5700@gmail.com>
2017-11-23 00:19:07 +0200

    update the jUnit test

---
+++ WIfi_Location2/src/CsvReaderTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+public class CsvReaderTest {
+
+	@Test
+	public void test() {
+		CsvReader cr = new CsvReader();
+		File f= new File("data\\WigleWifi_20171031190803.csv");
+		List<RawData>data = new ArrayList<RawData>();
+		f = null;
+		try {
+			data=cr.readCsv(f,data);
+		} catch (Exception e) {
+			assertFalse("failed: "+e.toString(),true);
+		}
+		assertTrue(data.size()>0);
+		String lastTime=null;
+
+		for(int i=0;i<data.size();i++) {
+			String currentTime = null;
+			currentTime=data.get(i).getSamples().get(0).getFirstSeen();
+			assertFalse(lastTime!=null&&lastTime.equals(currentTime));
+		}
+	}
+
+}
---
+++ WIfi_Location2/src/RawDataTest.java
@@ -0,0 +1,38 @@
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+
+class RawDataTest {
+
+	@Test
+	void testOrganizeData() throws Exception {
+		CsvReader cr = new CsvReader();
+		String folderPath="data";
+		List<RawData> data;
+		File folder = new File(folderPath);
+
+		File[] listOfFiles = folder.listFiles(new FilenameFilter(){
+			public boolean accept(File dir, String filename)
+			{ return filename.endsWith(".csv"); }
+		} );		
+		if(listOfFiles==null){
+			throw new Exception("The folder "+folderPath+" does not exist");
+		}
+		for (int i = 0; i < listOfFiles.length; i++) {
+			data = new ArrayList<RawData>();
+			cr.readCsv(listOfFiles[i],data);
+			data=RawData.organizeData(data);
+			for(int j=0;j<data.size();j++) {
+				assertTrue( "too many wifi spots",data.get(i).getSamples().size()<=10);
+			}
+		}
+	}
+	
+
+}
