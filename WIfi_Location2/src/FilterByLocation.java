44f6171a2c448e9cf7a76b0ea6e66ce43ddc7ec7
YairIvgi <yair5700@gmail.com>
2017-12-11 00:02:12 +0200

    fix return

--- WIfi_Location2/src/FilterByLocation.java
+++ WIfi_Location2/src/FilterByLocation.java
@@ -9,16 +9,16 @@ public class FilterByLocation implements IFilter {
 	 * This class filters the data by location.
 	 */
 	private double	m_lon;
- 	private double	m_lat;
- 	private double	m_radius;
+	private double	m_lat;
+	private double	m_radius;
 	/**
 	 * @author Yair Ivgi 
 	 * the builder gets the coordinates specified by the user. 
 	 */
 	public FilterByLocation(double lon,double lat,double radius) {
- 		m_lon=lon;
- 		m_lat=lat;
- 		m_radius=radius;
+		m_lon=lon;
+		m_lat=lat;
+		m_radius=radius;
 	}
 	/**
 	 * @author Yair Ivgi and Idan Hollander 
@@ -27,30 +27,30 @@ public class FilterByLocation implements IFilter {
 	@Override
 	public List<CSVRecord> getFiltered(Iterable<CSVRecord> records) throws Exception {
 		if(m_radius<0){
- 			throw new Exception("radius must be positive");
+			throw new Exception("radius must be positive");
 		}
 		List<CSVRecord> result = new ArrayList<CSVRecord>();
 		double currentLon;
 		double currentLat;
 		double dist;
 		for (CSVRecord record : records) {
-			 currentLat = Double.parseDouble(record.get("Lat"));
-			 currentLon = Double.parseDouble(record.get("Lon"));
-					 dist=Math.sqrt(Math.pow(currentLon-m_lon, 2)+Math.pow(currentLat-m_lat, 2));
- 					if(dist<=m_radius){
-							result.add(record);
-						}
-					}
+			currentLat = Double.parseDouble(record.get("Lat"));
+			currentLon = Double.parseDouble(record.get("Lon"));
+			dist=Math.sqrt(Math.pow(currentLon-m_lon, 2)+Math.pow(currentLat-m_lat, 2));
+			if(dist<=m_radius){
+				result.add(record);
+			}
 		}
 		return result;
 	}
+
 	public double getLon() {
- 		return this.m_lon;
+		return this.m_lon;
 	}
 	public double getLat() {
- 		return this.m_lat;
+		return this.m_lat;
 	}
 	public double getRadius() {
- 		return this.m_radius;
+		return this.m_radius;
 	}
 }
