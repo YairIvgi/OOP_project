6db5d9a9d1b37307a0a120998321f5e754fc93a9
YairIvgi <yair5700@gmail.com>
2017-12-31 11:30:37 +0200

    added a use unionRecord

--- WIfi_Location2/src/filter/OrFilters.java
+++ WIfi_Location2/src/filter/OrFilters.java
@@ -9,6 +9,8 @@ import java.util.List;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 
+import readAndWrite.UnionRecords;
+
 public class OrFilters implements IOperationFilter {
 
 	private String m_fileName;
@@ -23,10 +25,13 @@ public class OrFilters implements IOperationFilter {
 		Reader in = new FileReader(file);
 		Iterable<CSVRecord> records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
 		List<CSVRecord> filteredRecords1 = filter1.getFiltered(records);
+		in.close();
+		in = new FileReader(file);
+		records = CSVFormat.RFC4180.withFirstRecordAsHeader().parse(in);
 		List<CSVRecord> filteredRecords2 = filter2.getFiltered(records);
-		List<CSVRecord> combained = new ArrayList<CSVRecord>(filteredRecords1);
-		combained.addAll(filteredRecords2);
-		return combained;
+		UnionRecords unite = new UnionRecords(filteredRecords1);
+		unite.combainData(filteredRecords2);
+		return unite.getM_records();
 	}
 
 }
